3e39d8d3b8b4390a214e23a89d11d1223dd485fc
CTSham <https://github.com/CTSham>
2026-01-17 14:34:09 -0800

    updated project files

--- tests/test_priority.py
+++ tests/test_priority.py
@@ -3,5 +3,8 @@ from src.triage import calculate_priority
 def test_priority_p1():
     assert calculate_priority(1, 1) == "P1"
 
+def test_priority_p2():
+    assert calculate_priority(1, 2) == "P2"
+
 def test_priority_p4():
     assert calculate_priority(3, 3) == "P4"
